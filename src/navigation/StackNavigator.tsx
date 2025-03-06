d7f3fa1d7127fe82531fda084ceb6ea36ad87576
giahann <giahan05092003@gmail.com>
2025-03-06 11:03:50 +0700

    fix bug

--- src/navigation/StackNavigator.tsx
+++ src/navigation/StackNavigator.tsx
@@ -40,7 +40,7 @@ export type RootStackParamList = {
   LanguageChange: undefined;
   AllReviews: { productId: number };
   AddReview: { productId: number };
-  Cart:undefined;
+  Cart: undefined;
   ListVoucher: undefined
   HistoryOrders: undefined;
   DeliveringOrders: undefined;
@@ -60,7 +60,6 @@ const StackNavigator = () => {
   return (
     <Stack.Navigator initialRouteName="Login">
       <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-
       <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
       <Stack.Screen name="ForgotPassword" component={ForgotPassword} options={{ headerShown: false }} />
       <Stack.Screen
@@ -73,23 +72,23 @@ const StackNavigator = () => {
       />
 
       <Stack.Screen name="Info" component={Info} options={{ headerShown: false }} />
-      <Stack.Screen name="ProductDetail" component={ProductDetail}  options={{
-          headerShown: false,
-          animation: 'slide_from_right'  // Thử đổi thành 'slide_from_right' hoặc 'fade'
-        }} />
-         <Stack.Screen name="Cart" component={Cart}  options={{
-          headerShown: false,
-          animation: 'slide_from_right'  // Thử đổi thành 'slide_from_right' hoặc 'fade'
-        }} />
-        <Stack.Screen name="News" component={News} options={{ headerShown: false, animation: 'slide_from_right' }} />
+      <Stack.Screen name="ProductDetail" component={ProductDetail} options={{
+        headerShown: false,
+        animation: 'slide_from_right'  // Thử đổi thành 'slide_from_right' hoặc 'fade'
+      }} />
+      <Stack.Screen name="Cart" component={Cart} options={{
+        headerShown: false,
+        animation: 'slide_from_right'  // Thử đổi thành 'slide_from_right' hoặc 'fade'
+      }} />
+      <Stack.Screen name="News" component={News} options={{ headerShown: false, animation: 'slide_from_right' }} />
 
-        <Stack.Screen name="Order" component={OrderScreen} options={{ headerShown: false, animation: 'slide_from_right' }} />
-        <Stack.Screen name="NewDetails" component={NewDetails} options={{ headerShown: false, animation: 'slide_from_right' }} />
-        <Stack.Screen name="LanguageChange" component={LanguageChange} options={{ title: t('common.selectLanguage'), headerShown: false, animation: 'slide_from_right' as const }} />
+      <Stack.Screen name="Order" component={OrderScreen} options={{ headerShown: false, animation: 'slide_from_right' }} />
+      <Stack.Screen name="NewDetails" component={NewDetails} options={{ headerShown: false, animation: 'slide_from_right' }} />
+      <Stack.Screen name="LanguageChange" component={LanguageChange} options={{ title: t('common.selectLanguage'), headerShown: false, animation: 'slide_from_right' as const }} />
       <Stack.Screen name="AllReviews" component={AllReviewsScreen} options={{ title: t('common.allReviews'), headerShown: false, animation: 'slide_from_right' as const }} />
       <Stack.Screen name="AddReview" component={AddReviewScreen} options={{ title: t('common.addReview'), headerShown: false, animation: 'slide_from_right' as const }} />
       <Stack.Screen name="ListVoucher" component={ListVoucher} options={{ title: t('common.addReview'), headerShown: false, animation: 'slide_from_right' as const }} />      <Stack.Screen name="DeliveringOrders" component={DeliveringOrders} options={{ headerShown: false, animation: 'slide_from_right' }} />
-      <Stack.Screen name="PendingOrders" component={PendingOrders} options={{ headerShown: false, animation: 'slide_from_right' }}/>
+      <Stack.Screen name="PendingOrders" component={PendingOrders} options={{ headerShown: false, animation: 'slide_from_right' }} />
       <Stack.Screen name="HistoryOrders" component={HistoryOrders} options={{ headerShown: false, animation: 'slide_from_right' }} />
       <Stack.Screen name="CancelledOrders" component={CancelledOrders} options={{ headerShown: false, animation: 'slide_from_right' }} />
       <Stack.Screen name="WaitingOrders" component={WaitingOrders} options={{ headerShown: false, animation: 'slide_from_right' }} />
